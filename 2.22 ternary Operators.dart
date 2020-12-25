e99207a22cd0412b4501f61d0127aa64dc2c068a
snagurad <63306361+snagurad@users.noreply.github.com>
2020-12-25 18:16:40 +0530

    Create 2.22 ternary Operators.dart

---
+++ 2.22 ternary Operators.dart
@@ -0,0 +1,10 @@
+//2.22 ternary Operators
+//Conditional Operators
+// exp1(bool)?exp2(true):exp3(false)
+void main() {
+  
+  int age = 50;
+  String type = age < 18 ? 'Child' : 'Adult';
+  print(type);
+
+}
