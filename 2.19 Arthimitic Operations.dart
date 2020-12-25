6ad8ec508545033390a334f8c204cfee24f74366
snagurad <63306361+snagurad@users.noreply.github.com>
2020-12-25 17:54:15 +0530

    Create 2.19 Arthimitic Operations.dart

---
+++ 2.19 Arthimitic Operations.dart
@@ -0,0 +1,26 @@
+//2.19 Arthimitic Operations
+void main() {
+  //Arithmetic
+  // + - * / ~/(Int) %
+  print(5 + 2);
+  print(5 - 2);
+  print(5 * 2);
+  print(5 / 2);
+  print(5 ~/ 2);
+  print(5 % 2);
+
+  //Augmented
+  // += -= *= /= ~/= %=
+  int x = 5;
+  x *= 2;
+  print(x);
+  
+  //postfix increment decrement
+  // ++ --
+  x = 0;
+  x++;
+  print(x);
+  ++x;
+  print(x);
+  
+}
