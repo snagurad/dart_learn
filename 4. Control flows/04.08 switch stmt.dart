8f3aad53cc2865a6f57b1ffd69a790819f8fc1d1
snagurad <63306361+snagurad@users.noreply.github.com>
2020-12-26 11:49:21 +0530

    Create 04.08 switch stmt.dart

---
+++ 4. Control flows/04.08 switch stmt.dart
@@ -0,0 +1,18 @@
+//switch stmt
+void main() {
+  const pos = 2;
+  switch (pos) {
+    case 1:
+      print('Glod ');
+      break;
+    case 2:
+      print('Silver ');
+      break;
+    case 3:
+      print('Bronze ');
+      break;
+    default:
+      print('No Medal');
+      break;
+  }
+}
