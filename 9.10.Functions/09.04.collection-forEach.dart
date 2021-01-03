4ebec4eea1409f18799bcb4a4153225c7c10dbc3
snagurad <63306361+snagurad@users.noreply.github.com>
2021-01-03 18:36:09 +0530

    Create 09.04.collection-forEach.dart

---
+++ 9.10.Functions/09.04.collection-forEach.dart
@@ -0,0 +1,19 @@
+//Functions - Advanced
+//Methods or Opertions collection-forEach, map, where, reduce
+void main() {
+  const list = [1, 2, 3];
+  for (var value in list) {
+    print(value);
+  }
+  //simple and single line
+  list.forEach((name) => print(name));
+
+  list.forEach((name) {
+    //Some Complex Logics
+    print(name);
+  });
+
+  //Function can be passed as arugment to function as long as its same type
+  //you can pass existing named function
+  list.forEach(print);
+}
